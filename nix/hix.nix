f6eb791d8b71629dccd8405acb61e6548cc21768
Trouble-Truffle <perigordtruffle7318@gmail.com>
2023-03-30 13:13:00 +0800

    first commit

---
+++ nix/hix.nix
@@ -0,0 +1,24 @@
+{pkgs, ...}: {
+  name = "video";
+  compiler-nix-name = "ghc902"; # Version of GHC to use
+
+  # Tools to include in the development shell
+  shell.tools.cabal = "latest";
+  shell.tools.cabal-fmt = "latest";
+  shell.tools.hlint = "latest";
+  shell.tools.haskell-language-server = "1.8.0.0";
+
+  shell.buildInputs = with pkgs; [
+    nixpkgs-fmt
+    libGL
+    zlib
+    freeglut
+    povray
+    ffmpeg
+    glibc
+  ];
+
+  shell.shellHook = ''
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.freeglut}/lib"
+  '';
+}
