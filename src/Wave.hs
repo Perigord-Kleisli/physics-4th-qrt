36b384a30c625f05c5e2dec3516f614c49952e59
Trouble-Truffle <perigordtruffle7318@gmail.com>
2023-04-12 12:07:02 +0800

    Wed Apr 12 12:07:02 PM PST 2023

--- src/Wave.hs
+++ src/Wave.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE ParallelListComp #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ViewPatterns #-}
 
-module Wave (wave) where
+module Wave  where
 
 import           Codec.Picture
 import           Control.Lens
@@ -9,30 +10,57 @@ import           Data.Complex
 import           Graphics.SvgTree
 import           Linear.V2
 import           Reanimate
-import           Reanimate.Scene (oDraw)
+import           Reanimate.Scene (oDraw, oNew, oShowWith, oModify, oEasing
+                                , oFadeOut, oHideWith)
 import           Data.Text (pack)
 import           Reanimate.Animation
+import           Environment
+import           Control.Monad
+import           Util
+import           Control.Applicative
 
+waveSvg :: Float -> Float -> Tree
+waveSvg = undefined
+
+mkWave :: Double -> Double -> Double -> [(Double, Double)]
+mkWave amplitude phase frequency =
+  [(x, amplitude * sin (x * amplitude + phase)) | x <- [0, 0.01 ..]]
+
+nth :: Int -> [a] -> [a]
+nth _ [] = []
+nth n xs = let (x,xs') = splitAt n xs in head x : nth n xs'
+
+-- mkArrow :: (Double,Double) -> Tree
+-- mkArrow = mkLine ()
 wave :: IO ()
 wave = reanimate
   $ scene
     do
-      _ <- newSpriteSVG (mkBackgroundPixel (PixelRGBA8 10 10 10 0xFF))
-      width <- fork
-        $ newSpriteA' SyncFreeze
-        $ oDraw
-        $ translate 0 2
-        $ withFillColorPixel (PixelRGBA8 255 255 255 255)
-        $ mkText (pack $ show screenWidth)
-      height <- newSpriteA' SyncFreeze
-        $ oDraw
-        $ translate 0 (-2)
-        $ withFillColorPixel (PixelRGBA8 255 255 255 255)
-        $ mkText (pack $ show screenHeight)
-      play
-        $ setDuration 2
-        $ oDraw
-        $ withStrokeColorPixel (PixelRGBA8 0xFF 0xFF 0xFF 0xFF)
-        $ withFillOpacity 0
-        $ mkLinePath
-          [(x, sin x) | x <- [-8,-7.9..8]]
+      _ <- newSpriteSVG background
+      _ <- newSpriteSVG bgGrid
+      phase <- newVar 0
+      amplitude <- newVar 0
+      frequency <- newVar 0
+      _ <- do
+        newSprite
+          $ liftA3 (,,) (unVar phase) (unVar amplitude) (unVar frequency)
+          <&> \(phase', amplitude', frequency')
+          -> let waveVals = [(x, amplitude' * sin (x * frequency' + phase'))
+                            | x <- realRange (-8.0) 8.0]
+             in mkGroup
+                  (withStrokeColorPixel
+                     white
+                     (withStrokeWidth 0.1
+                      $ withStrokeOpacity 1
+                      $ withFillOpacity 0
+                      $ mkLinePath waveVals)
+                   :map
+                     (\elem -> withStrokeWidth 0.1
+                      $ withStrokeOpacity 1
+                      $ withStrokeColorPixel white
+                      $ mkLine (fst elem, 0) elem)
+                     (nth 20 waveVals))
+      fork $ tweenVar phase 4 (\x t -> (t * 20) + x)
+      fork $ tweenVar amplitude 2 (\x (curveS 2 -> t) -> (t * 3) + x)
+      tweenVar frequency 2 (\x (curveS 2 -> t) -> ((t * 3) + x) / 2)
+      wait 1
